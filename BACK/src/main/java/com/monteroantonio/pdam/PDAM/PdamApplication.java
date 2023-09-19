52bd16a0f53b23b987840d2ca2a7a73e65d3fd8a
Antonio Montero <antoniomonforma99@gmail.com>
2023-09-19 17:42:46 +0200

    packages

---
+++ BACK/src/main/java/com/monteroantonio/pdam/PDAM/PdamApplication.java
@@ -0,0 +1,13 @@
+package com.monteroantonio.pdam.PDAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdamApplication.class, args);
+	}
+
+}
---
+++ BACK/src/test/java/com/monteroantonio/pdam/PDAM/PdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monteroantonio.pdam.PDAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
